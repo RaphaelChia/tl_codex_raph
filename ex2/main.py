62426b8bdd98431a28824972badbf40079d7d033
Raphael Chia <raphael.chia@ncs.com.sg>
2022-09-05 22:14:18 +0800

    bugfix: ex2
    Negative numbers are now properly accounted for

--- ex2/main.py
+++ ex2/main.py
@@ -1,4 +1,3 @@
-
 '''
 @Author: Raphael Chia Song Zuo
 @Date: 5 September 2022
@@ -7,6 +6,7 @@
 # Assume that equation will always be in the simplest form, a {operand} b, 
 #   Never a {operand} b {operand} c    
 # Assuming no decimals in string arithmetic input
+
 
 class Node:
     def __init__(self,data=0.0,left=None,right=None):
@@ -30,30 +30,49 @@ def str_to_tree(s:str) -> Node:
             stack.append(Node(num))
             i-=1
         elif s[i]==')': 
+
             right_node = stack.pop() 
-            left_node = stack.pop(-2)
-            parent_node = stack[-1] 
-            
-            parent_node.right = right_node
-            parent_node.left = left_node
+            if stack[-2] in op_list:
+                # Checking if there are 2 arithmetic side by side
+                # accounting for negative numbers A.K.A (4/2)*-16
+                first_arithmetic_node = stack.pop() 
+                parent_node = stack[-1] # second arithmetic
+                left_node = stack.pop(-2)
+                parent_node.right = first_arithmetic_node
+                parent_node.left = left_node
+                first_arithmetic_node.right = right_node
+            else:    
+                left_node = stack.pop(-2)
+                parent_node = stack[-1] 
+                parent_node.right = right_node
+                parent_node.left = left_node
        
         i+=1
-
-    #need to handle for if there is no more brackets but still stuff left in the stack 
-    if stack[0].data not in op_list:
-        right_node = stack.pop() # This is o(1)
-        left_node = stack.pop(-2) # This is o(n)
+    # need to handle for if there is no more brackets but still stuff left in the stack 
+    if len(stack)==4:
+        # accounting for negative numbers
+        right_node = stack.pop() 
+        first_arithmetic_node = stack.pop() 
+        parent_node = stack[-1] # second arithmetic
+        left_node = stack.pop(-2)
+        parent_node.right = first_arithmetic_node
+        parent_node.left = left_node
+        first_arithmetic_node.right = right_node
+    elif len(stack)==3:
+        right_node = stack.pop() 
+        left_node = stack.pop(-2)
         parent_node = stack[-1] 
-        
         parent_node.right = right_node
         parent_node.left = left_node
+       
 
     return stack[0]
 def main():
     arithString = input("Input arithmetic string without spaces: ")
     headNode = str_to_tree(arithString)
-    print(headNode.data)
 
 
 if __name__ == "__main__":
     main()
+
+
