4163ba9c47682fe51f7e1feb2df522c56300ca85
Raphael Chia <raphael.chia@ncs.com.sg>
2022-09-06 21:13:56 +0800

    ex2v2 #wip
    infix to postfix is working already

--- ex2/constants.py
+++ ex2/constants.py
@@ -1,1 +1,7 @@
 op_list = {"+", "-", "/", "*"}
+
+
+# =====v2==== #
+Operators = set(['+','-','*','/','(',')'])  # unique collection of Operators
+Priority = {'+':1,'-':1,'*':2,'/':2} # dictionary having priorities of Operators, because * and / need to be evaluated first.
+
--- ex2/ex2v2.py
+++ ex2/ex2v2.py
@@ -4,3 +4,47 @@ IDEA:   To convert Arithmetic string expression >  postfix > binary tree > compu
         Just add it into the binary tree, and then 
 '''
 
+from constants import Operators, Priority
+
+
+class Node:
+    def __init__(self,data=0.0,left=None,right=None):
+        self.left = left
+        self.right = right
+        self.data = data
+
+
+def infixToPostfix(arithmeticString):
+    stack   = [] # initialization of empty stack
+    output  = ''
+    for character in arithmeticString:
+
+        # We know there are 4 scenarios. If its open bracket, close bracket, operator, or digit.
+        if character not in Operators:  # if digit, append in postfix expression
+            output+= character
+        elif character=='(':  # else Operators push onto stack
+            stack.append('(')
+        elif character==')': # closing bracket works differently, need to pop into output
+            while stack and stack[-1]!='(':
+                output+=stack.pop()
+            stack.pop() # This is to pop the opening bracket just before finishing
+        else: 
+            while stack and stack[-1]!='(' and Priority[character]<=Priority[stack[-1]]: # when reaches here, has to be operator already.
+                output+=stack.pop() # If the incoming operator is lower priority, the stack last item gets popped into postfix output
+            stack.append(character)
+        prev=character
+    # Handling the remaining operators in the stack            
+    while stack:
+        output+=stack.pop()
+    return output
+
+
+
+def main():
+    expression = input('Enter infix expression ')
+    postFixString = infixToPostfix(expression)
+    print(postFixString)
+
+
+if __name__ == "__main__":
+    main()
